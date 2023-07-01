1fc898c245d672109c9999c70a081de863257708
monaabishek2003 <mona2010898@ssn.edu.in>
2023-07-01 21:59:00 +0530

    Models Update 4

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,8 +3,25 @@ import { motion } from "framer-motion"
 
 import { styles } from "../styles"
 import { ComputersCanvas } from "./canvas"
+import { Suspense,useEffect,useState } from 'react';
 
-const Hero = () => {
+const Hero = () => 
+{  
+  const [isMobile,setisMobile] = useState(false);
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width:500px)');
+    
+    setisMobile(mediaQuery.matches);
+    
+    const handleMediaQueryChange = (event)=>{
+      setisMobile(event.matches);
+    }
+    mediaQuery.addEventListener('change',handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener('change',handleMediaQueryChange);
+    }
+  })
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
@@ -23,7 +40,12 @@ const Hero = () => {
           </p>
         </div>
       </div>
-      <ComputersCanvas/>
+      
+      {isMobile ? (
+                          <span/>
+                        ) : (
+                          <ComputersCanvas/>
+                        )}
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>

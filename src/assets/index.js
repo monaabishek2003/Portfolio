2ebfdad39821b1e5569e8debbdebb6b9c3214cdf
monaabishek2003 <mona2010898@ssn.edu.in>
2023-07-01 20:47:18 +0530

    Models Update

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,19 +7,15 @@ import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 
-import css from "./tech/css.png";
-import docker from "./tech/docker.png";
+import html from "./tech/html.jpg";
+import css from "./tech/css.jpeg";
 import figma from "./tech/figma.png";
-import git from "./tech/git.png";
-import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
+import git from "./tech/git.png";
 import mongodb from "./tech/mongodb.png";
 import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
-import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
 import express from "./tech/express.png";
 import restapi from "./tech/restapi.png"
 import python from "./tech/python.png"
@@ -30,9 +26,6 @@ import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
 
-import carrent from "./carrent.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
 
 import stock from "./stock.jpg";
 import pixshare from "./pixshare.png";
@@ -49,7 +42,6 @@ export {
   menu,
   close,
   css,
-  docker,
   figma,
   git,
   html,
@@ -57,10 +49,7 @@ export {
   mongodb,
   nodejs,
   reactjs,
-  redux,
-  tailwind,
   typescript,
-  threejs,
   express,
   restapi,
   python,
@@ -68,9 +57,6 @@ export {
   shopify,
   starbucks,
   tesla,
-  carrent,
-  jobit,
-  tripguide,
   stock,
   melthify,
   pixshare
